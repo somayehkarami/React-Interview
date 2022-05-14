5296f7226444005a7913ab74e741128e39498e60
Somayeh Karami <sk20206@yahoo.com>
2022-05-14 16:22:52 -0400

    *review chapter #17& #18

--- src/Home.js
+++ src/Home.js
@@ -5,25 +5,37 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  const [isPending, setPending] = useState(true);
+  const [isPending, setIsPending] = useState(true);
 
   /*const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };*/
 
+  //fetch the data once when the component first renders
+
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
+      fetch("http://localhost:8000/blogs") //endpoint from the json file(get request)
+        //this returns to us a promise we need to use then method
         .then((res) => {
-          return res.json();
+          //res is an object that has an ok property
+          if (!res.ok) {
+            throw Error("could not fetch the data :)");
+          }
+          return res.json(); //to get the data passes the jason into a javascript object(this is asynchronus it takes some times to do)
         })
         .then((data) => {
+          //takes another promise
           //console.log(data);
           setBlogs(data);
-          setPending(false);
+          setIsPending(false);
+        })
+        .catch((err) => {
+          //handel any network error
+          console.log(err.message);
         });
-    }, 1000);
+    }, 1000); //after one second perform the fetch
   }, []);
 
   return (
@@ -33,5 +45,14 @@ const Home = () => {
     </div>
   );
 };
+//null evaluates to false
 
 export default Home;
+//A Promise is in one of these states:
+
+//pending: initial state, neither fulfilled nor rejected.
+//fulfilled: meaning that the operation was completed successfully.
+//rejected: meaning that the operation failed.
+
+//If present, effect will only activate if the values in the list change.[]
+//Accepts a function that contains imperative, possibly effectful code.
